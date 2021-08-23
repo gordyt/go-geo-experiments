64af9083365821d766a3ba00fb9f082f85a400fa
Gordon Tillman <gordyt@gmail.com>
2021-08-23 10:03:49 -0500

    experiments

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -12,9 +12,9 @@ import (
 )
 
 var (
-	input = flag.String("--input-format", XYZ, "input format")
+	input  = flag.String("--input-format", XYZ, "input format")
 	output = flag.String("--output-format", ENU, "output format")
-	help = flag.Bool("--help", false, "if true, print help and exit")
+	help   = flag.Bool("--help", false, "if true, print help and exit")
 )
 
 const (
@@ -22,10 +22,8 @@ const (
 	LLA = "LLA"
 	XYZ = "XYZ"
 	ENU = "ENU"
-	valConstructs = map[string]func
+	//valConstructs = map[string]func
 )
-
-
 
 func usage() {
 	fmt.Println("TODO help")
@@ -38,10 +36,10 @@ func main() {
 		strings.ToUpper(*output),
 		flag.Args())
 	if len(flag.Args()) != 3 {
-		log.Println("[ERROR][main] expected 3 args, got %d", len(flag.Args()))
+		log.Printf("[ERROR][main] expected 3 args, got %d", len(flag.Args()))
 		os.Exit(1)
 	}
-	var args [3]float64{}
+	var args [3]float64
 	for i, arg := range flag.Args() {
 		val, err := strconv.ParseFloat(arg, 64)
 		if err != nil {
@@ -50,10 +48,22 @@ func main() {
 		}
 		args[i] = val
 	}
-	input := geo.XYZ{X: geo.Meters(args[0]), Y: geo.Meters(args[1]), Z: geo.Meters(args[2])}
+	// 161 Willow
+	ref := geo.LLA{
+		Latitude:  29.99504406861832,
+		Longitude: -95.25508907331802,
+		Altitude:  27.0,
+	}
+	log.Printf("[INFO][main] REF=%+v", ref)
+	xyz := geo.XYZ{X: geo.Meters(args[0]), Y: geo.Meters(args[1]), Z: geo.Meters(args[2])}
+	log.Printf("[INFO][main] XYZ=%+v", xyz)
 	wsg := geo.WGS84()
-	lla := wsg.XYZToLLA(input)
-	wsg.LLAToENU(lla)
-
+	log.Printf("[INFO][main] WSG=%+v", wsg)
+	enu := wsg.XYZToENU(ref, xyz)
+	log.Printf("[INFO][main] ENU=%+v", enu)
+	ned := wsg.XYZToNED(ref, xyz)
+	log.Printf("[INFO][main] NED=%+v", ned)
+	xyz2 := wsg.ENUToXYZ(ref, enu)
+	log.Printf("[INFO][main] xyz2 = %+v", xyz2)
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/gordyt/go-geo-experiments
 
 go 1.16
 
-require pault.ag/go/geo v0.1.1
+require pault.ag/go/geo v0.0.1
+
+replace pault.ag/go/geo v0.0.1 => /Users/gordy/Projects/g/go-geo
